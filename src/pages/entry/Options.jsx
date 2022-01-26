93d53ecf679ca3cb952ade252c626272048c844f
tejaswipandava <tejaswi.pandava@gmail.com>
2022-01-26 23:58:11 +0530

    error handling with test

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -3,20 +3,24 @@ import { useEffect, useState } from "react";
 import { Row } from "react-bootstrap";
 import ScoopOptions from "./ScoopOptions";
 import ToppingOptions from "./ToppingOptions";
+import AlertBanner from "../common/AlertBanner";
 
 export default function Options({ optionType }) {
     const [items, setItem] = useState([]);
+    const [error1, setError] = useState(false);
 
     //optionsType is 'scoop' or 'topping' comming from server    
     useEffect(() => {
         axios.get(`http://localhost:3030/${optionType}`)
             .then(response => setItem(response.data))
-            .catch(error => {
-                //todo error handling
+            .catch((error) => {
+                setError(true);
             })
     }, [optionType]);
 
-    //Todo: need to replace null with toppings
+    if (error1) {
+        return <AlertBanner />;
+    }
     const ItemComponent = optionType === 'scoops' ? ScoopOptions : ToppingOptions;
 
     const optionItems = items.map(item => (
